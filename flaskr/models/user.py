8f9d9f1f5c74eca5444d7f01211fb2d9afff7483
Klaudija Stefan <kstefan@tomware.it>
2024-05-13 21:02:41 +0200

    user.py fixed

--- flaskr/models/user.py
+++ flaskr/models/user.py
@@ -27,23 +27,23 @@ class User(db.Model, UserMixin):
     fk_user_update      = db.Column(db.Integer, db.ForeignKey('t_user.id'), default=0, nullable=False)
     last_update         = db.Column(db.DateTime, nullable=False, default='1970-01-01 01:00:00')
     
-    def __init__(self, role_id, m_or_f_id, name, surname, username, email, 
-                  fk_user_insert, fk_user_update, insert_date, 
-                  image = '', note = '', 
-                  active = True):
-        self.role_id            = role_id
-        self.m_or_f_id          = m_or_f_id
-        self.name               = name
-        self.surname            = surname
-        self.username           = username
-        self.email              = email
-        self.image              = image
-        self.note               = note
-        self.active             = active
-        self.fk_user_insert     = fk_user_insert
-        self.fk_user_update     = fk_user_update
-        self.insert_date        = insert_date
-        self.last_update        = datetime.now()
+    # def __init__(self, role_id, m_or_f_id, name, surname, username, email, 
+    #               fk_user_insert, fk_user_update, insert_date, 
+    #               image = '', note = '', 
+    #               active = True):
+    #     self.role_id            = role_id
+    #     self.m_or_f_id          = m_or_f_id
+    #     self.name               = name
+    #     self.surname            = surname
+    #     self.username           = username
+    #     self.email              = email
+    #     self.image              = image
+    #     self.note               = note
+    #     self.active             = active
+    #     self.fk_user_insert     = fk_user_insert
+    #     self.fk_user_update     = fk_user_update
+    #     self.insert_date        = insert_date
+    #     self.last_update        = datetime.now()
 
     # getter and setter for the attribute
 
