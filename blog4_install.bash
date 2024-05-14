8a40c8e8840290e75bec2386eca8b60048203297
Klaudija Stefan <kstefan@tomware.it>
2024-05-14 12:07:24 +0200

    add blog4_install.bash

---
+++ blog4_install.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this is the initialisation script for the blog4 project
+
+LOGFILE="./install.log" 
+echo -------- `date` --------->> $LOGFILE
+echo output of the blog4 initialisation script >> $LOGFILE
+echo ------------------------->> $LOGFILE
+
+
+
