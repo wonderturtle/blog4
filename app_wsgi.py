427c83bf5dead4ff2df265feafcbf5e7244b586a
Klaudija Stefan <67047249+wonderturtle@users.noreply.github.com>
2024-05-14 11:21:44 +0200

    Update app_wsgi.py

--- app_wsgi.py
+++ app_wsgi.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.insert(0, '/var/www/vhosts/blog2.tomware.it/httpdocs/app')
+sys.path.insert(0, '/var/www/vhosts/blog4.tomware.it/httpdocs/app')
 
 from app import app as application
